c462645822e8007989f7199acdb68569e7696779
chagitlb <61159734+chagitlb@users.noreply.github.com>
2020-03-08 13:06:21 +0200

    1

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-#add fix to exercise5-server1 here
+sudo sed -i -e '$a\StrictHostKeyChecking no' /etc/ssh/ssh_config
+ssh-keygen -t rsa
+echo "vagrant" | sudo -S ssh-copy-id -i /etc/ssh/ssh_host_ecdsa_key.pub root@192.168.100.11
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-#add fix to exercise5-server2 here
+sudo sed -i -e '$a\StrictHostKeyChecking no' /etc/ssh/ssh_config
+
+ssh-keygen -t rsa
+echo "vagrant" | sudo -S ssh-copy-id -i /etc/ssh/ssh_host_ecdsa_key.pub root@192.168.100.10
